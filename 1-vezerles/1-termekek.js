a51943defd7e365d2c01413be5280b7470b7868e
farkas.norbert <farkas.norbert@ckik.hu>
2026-09-04 09:52:36 +0200

    1-termekek adatok

---
+++ 1-vezerles/1-termekek.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        name: "Televízió",
+        price: 80000,
+        category: "Elektronika"
+    },
+    {
+        name: "Hűtőszekrény",
+        price: 120000,
+        category: "Háztartási gépek"
+    },
+    {
+        name: "Mosógép",
+        price: 100000,
+        category: "Háztartási gépek"
+    },
+    { 
+        name: "Laptop",
+        price: 150000,
+        category: "Elektronika"
+    },
+    {
+        name: "Kávéfőző",
+        price: 25000,
+        category: "Konyhai eszközök"
+    }
+];
+
